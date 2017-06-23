4ce25522929b0440cf9cd8dd0a1a9ef60ab860ae
jdlawrence <jamil.lawrence@gmail.com>
2017-06-23 16:26:57 -0700

    it continues

--- SSE.js
+++ SSE.js
@@ -1,18 +1,47 @@
 const express = require('express');
 const app = express();
 const request = require('request');
+const fs = require('fs');
+const wstream = fs.createWriteStream('data.txt');
 
-// request.get('http://live-test-scores.herokuapp.com/scores').pipe(process.stdout);
-var body = [];
+function formatChunk(chunk) {
+  let splitChunk = chunk.toString().split('\n\n');
+  let result = [];
+  splitChunk.forEach(score => {
+    let beginning = score.indexOf('{');
+    let end = score.indexOf('}');
+    let scoreJSON = score.substring(beginning, end + 1);
+    result.push(scoreJSON);
+  });
+  return result;
+}
+let body = [];
 request.get('http://live-test-scores.herokuapp.com/scores')
   .on('data', (chunk) => {
-    body.push(chunk);
+    let formattedChunk = formatChunk(chunk);
+    body = body.concat(formattedChunk);
   });
 
-setTimeout(function () {
-  console.log(body.toString().split('\n\n'));
+function calculateStats(scores) {
+  let students = {};
+  let exams = {};
+  scores.forEach(score => {
+    students[scores] = students[scores] || [];
+    students[scores].push(score);
+  });
+  console.log('&&&&&&&&&&&&&&', students.scores);
+}
 
-}, 15000);
+setTimeout(function () {
+  calculateStats(body);
+  body.forEach(score => {
+    if (score.length > 0) {
+      wstream.write(score + '\n');
+      console.log('scoreJSON', score);
+    }
+  });
+}, 10000);
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
